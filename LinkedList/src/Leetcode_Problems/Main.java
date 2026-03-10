316322a102a9213443f2a3077488229dff56debe
likhi <likhithhn31@gmail.com>
2026-03-10 18:08:36 +0530

    JAVA DSA | Linked List | Leetcode Problem 1

---
+++ LinkedList/src/Leetcode_Problems/Main.java
@@ -0,0 +1,16 @@
+package Leetcode_Problems;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(1);
+        linkedList.append(2);
+        linkedList.append(3);
+        linkedList.append(4);
+        linkedList.append(5);
+        linkedList.append(6);
+
+        LinkedList.Node node = linkedList.findMiddleNode();
+
+        System.out.println(node.value);
+    }
+}
